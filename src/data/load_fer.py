6fb88377a3bd12419e2039964f7b8366df8c013b
pawan-nandakishore <pawan.nandakishore@gmail.com>
2020-06-08 11:19:48 -0700

    latest update

---
+++ src/data/load_fer.py
@@ -0,0 +1,51 @@
+
+import os 
+import tensorflow as tf
+
+
+
+def get_label(file_path):
+  # convert the path to a list of path components
+    parts = tf.strings.split(file_path, os.path.sep)
+  # The second to last is the class-directory
+    label = str(tf.strings.split(parts[-2]," ")[-1])
+    return label 
+
+def decode_img(img):
+  # convert the compressed string to a 3D uint8 tensor
+  img = tf.image.decode_png(img, channels=3)
+  # Use `convert_image_dtype` to convert to floats in the [0,1] range.
+  img = tf.image.convert_image_dtype(img, tf.float32)
+  # resize the image to the desired size.
+  return tf.image.resize(img, [IMG_HEIGHT, IMG_WIDTH])
+
+
+
+
+def process_path(file_path):
+    label = get_label(file_path)
+  # load the raw data from the file as a string
+    img = tf.io.read_file(file_path)
+    img = decode_img(img)
+    return img, label
+
+
+
+
+def load_fer():
+    raw_data = "../data/raw/fer2013"
+    train_location = os.path.join(raw_data,'train/')
+
+    AUTOTUNE = tf.data.experimental.AUTOTUNE
+    list_ds = tf.data.Dataset.list_files("../data/raw/fer2013/train/*/*", seed=1)
+    class_ds = tf.data.Dataset.list_files("../data/raw/fer2013/train/*", seed=1)
+
+    CLASS_NAMES = [str(x).split('/')[-1].strip("'").split(" ")[-1] for x in list(class_ds.as_numpy_iterator())]
+    IMG_HEIGHT = 32
+    IMG_WIDTH = 32
+
+    # Set `num_parallel_calls` so multiple images are loaded/processed in parallel.
+    labeled_ds = list_ds.map(process_path, num_parallel_calls=AUTOTUNE)
+
+    return labeled_ds
+
